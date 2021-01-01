d1c90183e3f0e856d5f72dc28690d12d579a5b3f
FabioPerez <65570296+fanpero87@users.noreply.github.com>
2021-01-01 17:06:47 -0500

    Add files via upload
    Replacing settings.py

--- wiki/wiki/settings.py
+++ wiki/wiki/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^ojf3#4u$p&wz5t0rgp_1e^4(y)l^vxg#g0nic$#wdv2-fqh*a'
+SECRET_KEY = '=xmrjk&y69ewwzofj8yj7=f*xt!#(3x1zzrip=vvg)gqfkm7n7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cs50w-django-wiki.herokuapp.com', 'localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'wiki.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dep3jdn1s3bglq',
+        'HOST': 'ec2-52-5-136-183.compute-1.amazonaws.com',
+        'PORT': 5432,
+        'USER': 'grfcicgsherhkf',
+        'PASSWORD': '2fa92c2bc7478e8313dc4587a78cdb4e1f2810af7e93474cc414a3e9c6615deb'
     }
 }
 
@@ -119,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
